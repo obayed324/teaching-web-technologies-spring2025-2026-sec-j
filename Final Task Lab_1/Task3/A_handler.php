fefc3e0319a08bcb04f412a13b861568b369e908
obayed324 <23-52090-2@student.aiub.edu>
2026-04-12 14:38:49 +0600

    TASK 3 A

---
+++ Final Task Lab_1/Task3/A_handler.php
@@ -0,0 +1,19 @@
+<?php
+
+    if(isset($_REQUEST['submit'])){
+
+        $dd   = $_REQUEST['dd'];
+        $mm   = $_REQUEST['mm'];
+        $yyyy = $_REQUEST['yyyy'];
+
+        if($dd == "" || $mm == "" || $yyyy == ""){
+            echo "Date of Birth fields are empty!";
+        }else{
+            echo "Date of Birth: " . $dd . "/" . $mm . "/" . $yyyy;
+        }
+
+    }else{
+        echo "Please submit the form...";
+    }
+
+?>
